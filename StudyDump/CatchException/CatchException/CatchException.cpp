cc7e690b3dadabd5d374a4bee347a1e56b4ddf53
LostBeca_se <876030131@qq.com>
2021-05-18 00:15:42 +0800

    mod: Add README.md

--- StudyDump/CatchException/CatchException/CatchException.cpp
+++ StudyDump/CatchException/CatchException/CatchException.cpp
@@ -1,78 +1,101 @@
 // CatchException.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include<Windows.h>
+#include<vector>
 #include <iostream>
 #include<DbgHelp.h>
 
 
 int GenerateMiniDump(PEXCEPTION_POINTERS pExceptionPointers)
 {
-    printf("Enter GenerateMiniDump\r\n");
-    // 定义函数指针
-    typedef BOOL(WINAPI* MiniDumpWriteDumpT)(
-        HANDLE,
-        DWORD,
-        HANDLE,
-        MINIDUMP_TYPE,
-        PMINIDUMP_EXCEPTION_INFORMATION,
-        PMINIDUMP_USER_STREAM_INFORMATION,
-        PMINIDUMP_CALLBACK_INFORMATION
-        );
-    // 从 "DbgHelp.dll" 库中获取 "MiniDumpWriteDump" 函数
-    MiniDumpWriteDumpT pfnMiniDumpWriteDump = NULL;
-    HMODULE hDbgHelp = LoadLibrary(L"DbgHelp.dll");
-    if (NULL == hDbgHelp)
-    {
-        printf("LoadLibrary DbgHelp.dll Failed!!!\r\n");
-        return EXCEPTION_CONTINUE_EXECUTION;
-    }
-    pfnMiniDumpWriteDump = (MiniDumpWriteDumpT)GetProcAddress(hDbgHelp, "MiniDumpWriteDump");
+	printf("Enter GenerateMiniDump\r\n");
+	// 定义函数指针
+	typedef BOOL(WINAPI* MiniDumpWriteDumpT)(
+		HANDLE,
+		DWORD,
+		HANDLE,
+		MINIDUMP_TYPE,
+		PMINIDUMP_EXCEPTION_INFORMATION,
+		PMINIDUMP_USER_STREAM_INFORMATION,
+		PMINIDUMP_CALLBACK_INFORMATION
+		);
+	// 从 "DbgHelp.dll" 库中获取 "MiniDumpWriteDump" 函数
+	MiniDumpWriteDumpT pfnMiniDumpWriteDump = NULL;
+	HMODULE hDbgHelp = LoadLibrary(L"DbgHelp.dll");
+	if (NULL == hDbgHelp)
+	{
+		printf("LoadLibrary DbgHelp.dll Failed!!!\r\n");
+		return EXCEPTION_CONTINUE_EXECUTION;
+	}
+	pfnMiniDumpWriteDump = (MiniDumpWriteDumpT)GetProcAddress(hDbgHelp, "MiniDumpWriteDump");
 
-    if (NULL == pfnMiniDumpWriteDump)
-    {
-        printf("Get Process Address Of MiniDumpWriteDump Failed!!!\r\n");
-        FreeLibrary(hDbgHelp);
-        return EXCEPTION_CONTINUE_EXECUTION;
-    }
-    // 创建 dmp 文件件
-    TCHAR szFileName[MAX_PATH] = { 0 };
-    TCHAR* szVersion = (TCHAR*)L"DumpDemo_v1.0";
-    SYSTEMTIME stLocalTime;
-    GetLocalTime(&stLocalTime);
-    wsprintf(szFileName, L"%s-%04d%02d%02d-%02d%02d%02d.dmp",
-        szVersion, stLocalTime.wYear, stLocalTime.wMonth, stLocalTime.wDay,
-        stLocalTime.wHour, stLocalTime.wMinute, stLocalTime.wSecond);
-    HANDLE hDumpFile = CreateFileW(szFileName, GENERIC_READ | GENERIC_WRITE,
-        FILE_SHARE_WRITE | FILE_SHARE_READ, 0, CREATE_ALWAYS, 0, 0);
-    if (INVALID_HANDLE_VALUE == hDumpFile)
-    {
-        printf("Create Dump File Failed!!!\r\n");
-        FreeLibrary(hDbgHelp);
-        return EXCEPTION_CONTINUE_EXECUTION;
-    }
-    // 写入 dmp 文件
-    MINIDUMP_EXCEPTION_INFORMATION expParam;
-    expParam.ThreadId = GetCurrentThreadId();
-    expParam.ExceptionPointers = pExceptionPointers;
-    expParam.ClientPointers = FALSE;
-    pfnMiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(),
-        hDumpFile, MiniDumpWithFullMemory, (pExceptionPointers ? &expParam : NULL), NULL, NULL);
-    // 释放文件
-    CloseHandle(hDumpFile);
-    FreeLibrary(hDbgHelp);
+	if (NULL == pfnMiniDumpWriteDump)
+	{
+		printf("Get Process Address Of MiniDumpWriteDump Failed!!!\r\n");
+		FreeLibrary(hDbgHelp);
+		return EXCEPTION_CONTINUE_EXECUTION;
+	}
+	// 创建 dmp 文件件
+	TCHAR szFileName[MAX_PATH] = { 0 };
+	TCHAR* szVersion = (TCHAR*)L"DumpDemo_v1.0";
+	SYSTEMTIME stLocalTime;
+	GetLocalTime(&stLocalTime);
+	wsprintf(szFileName, L"%s-%04d%02d%02d-%02d%02d%02d.dmp",
+		szVersion, stLocalTime.wYear, stLocalTime.wMonth, stLocalTime.wDay,
+		stLocalTime.wHour, stLocalTime.wMinute, stLocalTime.wSecond);
+	HANDLE hDumpFile = CreateFileW(szFileName, GENERIC_READ | GENERIC_WRITE,
+		FILE_SHARE_WRITE | FILE_SHARE_READ, 0, CREATE_ALWAYS, 0, 0);
+	if (INVALID_HANDLE_VALUE == hDumpFile)
+	{
+		printf("Create Dump File Failed!!!\r\n");
+		FreeLibrary(hDbgHelp);
+		return EXCEPTION_CONTINUE_EXECUTION;
+	}
+	// 写入 dmp 文件
+	MINIDUMP_EXCEPTION_INFORMATION expParam;
+	expParam.ThreadId = GetCurrentThreadId();
+	expParam.ExceptionPointers = pExceptionPointers;
+	expParam.ClientPointers = FALSE;
+	pfnMiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(),
+		hDumpFile, MiniDumpWithFullMemory, (pExceptionPointers ? &expParam : NULL), NULL, NULL);
+	// 释放文件
+	CloseHandle(hDumpFile);
+	FreeLibrary(hDbgHelp);
 
-    printf("Leave GenerateMiniDump\r\n");
-    return EXCEPTION_EXECUTE_HANDLER;
+	printf("Leave GenerateMiniDump\r\n");
+	return EXCEPTION_EXECUTE_HANDLER;
 }
 
 LONG WINAPI ExceptionFilter(LPEXCEPTION_POINTERS lpExceptionInfo)
 {
-    // 这里做一些异常的过滤或提示
-    if (IsDebuggerPresent())
-    {
-        return EXCEPTION_CONTINUE_SEARCH;
-    }
-    return GenerateMiniDump(lpExceptionInfo);
+	// 这里做一些异常的过滤或提示
+	//if (IsDebuggerPresent())
+	//{
+	//	return EXCEPTION_CONTINUE_SEARCH;
+	//}
+	return GenerateMiniDump(lpExceptionInfo);
+}
+
+void DisableSetUnhandledExceptionFilter()
+{
+	HMODULE _hModule = LoadLibraryW(L"kernel32.dll");
+	void* addr = (void*)GetProcAddress(_hModule, "SetUnhandledExceptionFilter");
+	BOOL _bRet = FALSE;
+	if (addr)
+	{
+		unsigned char code[16];
+		int size = 0;
+		code[size++] = 0x33;
+		code[size++] = 0xC0;
+		code[size++] = 0xC2;
+		code[size++] = 0x04;
+		code[size++] = 0x00;
+		DWORD dwOldFlag, dwTempFlag;
+		_bRet = VirtualProtect(addr, size, PAGE_READWRITE, &dwOldFlag);
+		_bRet = WriteProcessMemory(GetCurrentProcess(), addr, code, size, NULL);
+		_bRet = VirtualProtect(addr, size, dwOldFlag, &dwTempFlag);
+	}
+	FreeLibrary(_hModule);
 }
 
 
@@ -81,74 +104,92 @@ void MakeException02();
 void MakeException03();
 void MakeException04();
 void MakeException05();
-void MakeException06();
-void MakeException07();
 
-
-int main()
+int main(int nArgs_, char* pszArgs_[])
 {
-    // 加入崩溃dump文件功能
-    SetUnhandledExceptionFilter(ExceptionFilter);
+	if (nArgs_ < 2)
+	{
+		MakeException02();
+		return -1;
+	}
+	// 加入崩溃dump文件功能
+	SetUnhandledExceptionFilter(ExceptionFilter);
+	//DisableSetUnhandledExceptionFilter();
 
-    MakeException07();
-    std::cout << "No Exception!" << std::endl;
-    int i;
-    std::cin >> i;
-    return 0;
+	int _nSelection = atoi(pszArgs_[1]);
+	switch (_nSelection)
+	{
+	case 1:
+	{
+		MakeException01();
+		break;
+	}
+	case 2:
+	{
+		MakeException02();
+		break;
+	}
+	case 3:
+	{
+		MakeException03();
+		break;
+	}
+	case 4:
+	{
+		MakeException04();
+		break;
+	}
+	case 5:
+	{
+		MakeException05();
+	}
+	default:
+		break;
+	}
+	std::cout << "No Exception!" << std::endl;
+	int i;
+	std::cin >> i;
+	return 0;
 }
 
 void MakeException01()
 {
-    //访问空指针
-    int* _pInt = NULL;
-    *_pInt = 1;
+	//访问空指针
+	int* _pInt = NULL;
+	*_pInt = 1;
 }
 
 void MakeException02()
 {
-    int* _pInt = new int[1024];
+	int* _pInt = new int[1024];
 
-    _pInt[0] = 1;
-    _pInt[1] = 1;
-    for (int i = 2; i < 1024; i++)
-    {
-        _pInt[i] = _pInt[i - 1] + _pInt[i - 2];
-    }
-    printf("%d\r\n", _pInt[1023]);
-    delete[] _pInt;
-    delete[] _pInt;
+	_pInt[0] = 1;
+	_pInt[1] = 1;
+	for (int i = 2; i < 1024; i++)
+	{
+		_pInt[i] = _pInt[i - 1] + _pInt[i - 2];
+	}
+	printf("%d\r\n", _pInt[1023]);
+	delete[] _pInt;
+	delete[] _pInt;
 }
 
 void MakeException03()
 {
-    int i = 0;
-    for (int i = 0; i < 100; i++)
-    {
-        byte* _pStart = new byte[1024];
-        memcpy(_pStart - 512, _pStart, 1024);
-        delete[] _pStart;
-        std::cout << i << std::endl;
-    }
+	int i = 0;
+	byte* _pStart = new byte[1024];
+	memcpy(_pStart - 512, _pStart, 1024);
+	delete[] _pStart;
+	std::cout << i << std::endl;
 }
 
 void MakeException04()
 {
-    double _nNumber = 123.456;
-    double _nDivision = 0;
-    printf("%lf\r\n", _nNumber / _nDivision);
+	printf(nullptr);
 }
 
 void MakeException05()
 {
-    printf(nullptr);
-}
-
-void MakeException06()
-{
-    sqrt(-1);
-}
-
-void MakeException07()
-{
-    MessageBoxA((HWND)0x12345FF, "Hello world", "Hello world", 0x12345FF);
+	std::vector<int> _Vec;
+	_Vec.at(0);
 }
---
+++ StudyDump/CatchException/README.md
@@ -0,0 +1,28 @@
+# Dump
+
+程序崩溃时记录dump，方便进行调试。
+
+普通异常比较好抓dump，但是对于一些特殊的异常普通的方式抓不到。比如堆损坏异常，如果是重复释放还好办，因为是在重复释放的时候触发的。但是有时候堆栈被野指针写乱了，连基本信息都没了，普通的抓Dump的方式（调用`Kernel32.dll`中的`SetUnhandledExceptionFilter`)似乎不太管用了。即使抓到了也不是第一现场。
+
+针对上述情况可以考虑配置Windows注册表，使应用程序在`Crash`时，操作系统自动生成Dump
+
+## Windows Error Reporting
+
+Crash dumps are very useful to debug an application. Recently, I worked on a Visual Studio extension we use in my company. It's very easy to develop such kind of application. However, there are lots of reasons for your extension to crash. Of course, you can add lots of try/catch, but you'll for sure forget the good one, so VS will crash. When it's happening, you would like to be able to attach a debugger and see the exception and the stack trace. Instead of attaching a debugger, you can automatically generate a crash dump that you can use to debug the application later.
+
+In Windows, you can configure Windows Error Reporting (WER) to generate a dump when an application crashes.
+
+1. Open `regedit.exe`
+2. Open the key `HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\Windows Error Reporting\LocalDumps`
+3. Set the value `DumpFolder` (REG_EXPAND_SZ) to the directory you want the dump to be created
+4. Optionally, you can prevent WER to keep lots of crash dumps by setting `DumpCount` (DWORD) to a low number
+
+Maybe you prefer to set the configuration using PowerShell:
+
+```powershell
+New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\Windows Error Reporting" -Name "LocalDumps"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\Windows Error Reporting\LocalDumps" -Name "DumpFolder" -Value "%LOCALAPPDATA%\CrashDumps" -PropertyType "ExpandString"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\Windows Error Reporting\LocalDumps" -Name "DumpCount" -Value 10 -PropertyType DWord
+```
+
+You can also configure WER per application. So, if you want to generate a full dump only for one application, you can create a key for your application under `LocalDumps` with the configuration you want. If your application is `devenv.exe` the key name is `devenv.exe`. This is very simple!
